f7e358219ef07121ec9e7d722a24ead452336cce
Richard <richard.wigley@gmail.com>
2022-02-13 17:21:36 +0000

    TW: custom font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Kumbh Sans', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
 }
