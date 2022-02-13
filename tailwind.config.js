1e75a5baf4ca3e758192e5285cd67c14663dd42e
Richard <richard.wigley@gmail.com>
2022-02-13 18:16:27 +0000

    TW: custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'secondary-gray': {
+         100: 'var(--color-secondary-gray-100)',
+         200: 'var(--color-secondary-gray-200)',
+         300: 'var(--color-secondary-gray-300)',
+         400: 'var(--color-secondary-gray-400)',
+        },
+        'violet': {
+          100: 'var(--color-violet-100)',
+          300: 'var(--color-violet-300)',
+          400: 'var(--color-violet-400)',
+          500: 'var(--color-violet-500)',
+        }
+      },
       fontFamily: {
         sans: ['Kumbh Sans', ...defaultTheme.fontFamily.sans]
       }
