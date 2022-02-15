1efe3f56fdc6328bbc3cb5020ed7460df1823817
Richard <richard.wigley@gmail.com>
2022-02-15 18:50:57 +0000

    Devise username added

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { FFaker::Internet.unique.email }
+    username { FFaker::Internet.unique.user_name }
     password { "password" }
     password_confirmation { "password" }
   end
