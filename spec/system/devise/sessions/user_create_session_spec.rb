a58e672275ed114b8b618e8871f4eff101c02879
Richard <richard.wigley@gmail.com>
2022-02-16 16:12:24 +0000

    Devise session#new

---
+++ spec/system/devise/sessions/user_create_session_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Devise::Sessions::CustomerCreateSession", type: :system do
+  it "signs into the app with email" do
+    user = create(:user)
+    visit new_user_session_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
+
+    expect(page).to have_text "Signed in successfully."
+  end
+
+  it "displays error if sign in fails" do
+    create(:user)
+    visit new_user_session_path
+
+    click_on "Sign in"
+
+    expect(page).to have_text "Invalid Email or password."
+  end
+end
